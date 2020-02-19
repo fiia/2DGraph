0f2519728554385b3928750621751f8318ad1b55
Josefiina <josefiina.virtanen@tuni.fi>
2020-02-19 17:59:57 +0200

    Modified Edge

--- Edge.java
+++ Edge.java
@@ -1,4 +1,21 @@
 /*
-Josefiina
+*
+* Josefiina
+*
 */
 
+public class Edge {
+    private double len;
+    
+    public Edge(double len) {
+        this.len=len;
+    }
+    
+    public double getLen() { return this.len; }
+    
+    @Override
+    public String toString() {
+        return String.format("distance: %f", this.len);
+    }
+    
+}
